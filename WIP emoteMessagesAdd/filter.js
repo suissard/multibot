10318f4867830b298a7bd5fc0caa857201acaf8b
suissard <clavier.jeremy@gmail.com>
2025-12-18 20:15:20 +0100

    chore: remove WIP emote message filters and temporary verification scripts and images.

--- WIP emoteMessagesAdd/filter.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(messageReaction, user) {
-
-    if (user.bot) return false;
-    if (bot.EmoteMessages.get(messageReaction.message.id) == undefined) return false;
-    if (bot.EmoteMessages.get(messageReaction.message.id).channel.id != messageReaction.message.channel.id) return false;
-    return true;
-
-};
